ef3d5bacae15862e4601b7c1225ad8ff12dd955d
Jakerjohnson94 <jacobrichardjohnson94@gmail.com>
2018-04-24 17:41:59 -0400

    --noedit

--- script.js
+++ script.js
@@ -1,61 +1,60 @@
-function add(a, b){
-    return a+b;
+function add(a, b) {
+  return a + b;
 }
-console.log(add(2,4));
+console.log("add 2,4: " + add(2, 4));
 
-function multiply(a,b){
-    var result = 0;
-    for (i=1; i<=b; i++){
-        result = add(result, a) ;
+function multiply(a, b) {
+  var result = 0;
+  for (i = 1; i <= b; i++) {
+    result = add(result, a);
+  }
+  return result;
+}
+console.log("multiply 6,8: " + multiply(6, 8));
+
+function power(x, n) {
+  let i;
+  var result = 0;
+  for (i = 1; i < n; i++) {
+    while (i === 1) {
+      result = multiply(x, x);
+      i++;
     }
-    return result;
+    result = multiply(result, x);
+  }
+  return result;
 }
-console.log(multiply(6,8));
+console.log("power 2,8: " + power(2, 8));
 
+function factorial(a) {
+  let i;
+  var result = 1;
+  for (i = 1; i <= a; i++) {
+    result = multiply(result, i);
+  }
+  return result;
+}
+console.log("factoral 4: " + factorial(4));
 
-function power(x, n){
-    var i;
-    var result =  0 ;
-    for(i=1; i<n; i++){
-        while (i===1){
-            result = multiply(x,x);
-            i++;
-        }
-        result= multiply(result, x);
+function fibonacci(n) {
+  let i;
+  var p1 = 0;
+  var p2 = 0;
+  var result = 0;
+  for (i = 0; i < n; i++) {
+    if (i === 0) {
+      result = 0;
+      p1 = result;
     }
-    return result;
-}
-console.log(power(2,8)); 
-
-function factorial(a){
-    var i;
-    var result=1;
-    for(i=1; i<=a; i++){
-        result = multiply(result, i);
+    if (i === 1) {
+      result = 1;
+      p2 = result;
+    } else {
+      result = add(p1, p2);
+      p1 = p2;
+      p2 = result;
     }
-    return result;
+  }
+  return result;
 }
-console.log(factorial (4)); 
-
-function fibonacci(n){
-    var i;
-    var p1 = 0;
-    var p2 = 0;
-    var result =0;
-    for (i=0; i<n; i++){
-        if(i===0){
-            result = 0;
-            p1 = result;
-        }
-        if(i===1){
-            result = 1;
-            p2 = result;
-        } else{
-        result = add(p1,p2);
-        p1 = p2;
-        p2 = result;
-        }
-    }
-    return result;
-}
-console.log(fibonacci(8)); 
+console.log("fibonacci 8: " + fibonacci(8));
